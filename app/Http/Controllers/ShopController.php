22478dd7a44a1c65aeafa5e2aa46c7c7e0470aea
Adriaan Waem <dknightadri@live.nl>
2019-11-08 10:33:01 +0100

    Opdracht 5: <= Shop: detail page

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -37,9 +37,7 @@ class ShopController extends Controller
 
     public function index_alt(Request $request){
         $genres = Genre::has('records')->orderBy('name')->get()->transform(function($item, $key){
-            // Set first letter of name to uppercase and add the counter
             $item->name = ucfirst($item->name);
-            // Remove all fields that you don't use inside the view
             unset($item->created_at, $item->updated_at, $item->records_count);
             return $item;
         });
@@ -52,6 +50,16 @@ class ShopController extends Controller
     // Detail Page: http://vinyl_shop.test/shop/{id} or http://localhost:3000/shop/{id}
     public function show($id)
     {
-        return view('shop.show', ['id' => $id]);
+        $record = Record::with('genre')->findOrFail($id);
+        $record->cover = $record->cover ?? "https://coverartarchive.org/release/$record->title_mbid/front-250.jpg";
+        $record->title = $record->artist . ' - ' . $record->title;
+        $record->artistUrl = 'https://musicbrainz.org/ws/2/artist/' . $record->artist_mbid . '?inc=url-rels&fmt=json';
+        $record->recordUrl = 'https://musicbrainz.org/ws/2/release/' . $record->title_mbid . '?inc=recordings+url-rels&fmt=json';
+        $record->btnClass = $record->stock > 0 ? 'btn-outline-success' : 'btn-outline-danger';
+        $record->genreName = $record->genre->name;
+        unset($record->genre_id, $record->artist, $record->created_at, $record->updated_at, $record->artist_mbid, $record->title_mbid, $record->genre);
+        $result = compact('record');
+        \Json::dump($result);
+        return view('shop.show', ['record' => $record]);
     }
 }
